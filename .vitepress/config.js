7bc31e09caad959edf788f27ea26b5cd239a0863
QuinnMercer <zlz799777@gmail.com>
2026-03-27 14:57:05 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,28 +1,10 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "我的开发日志",
-  description: "记录Django+Vue开发与成长",
+export default {
+  title: '我的学习日志',
+  description: 'Django+Vue 开发记录',
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { text: '首页', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/QuinnMercer/my-learning-blog' }
     ]
   }
-})
+}
